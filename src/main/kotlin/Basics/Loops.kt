63b15e750f5d6220f2620d595e979595bf32de67
EricSeokgon <leesk55@gmail.com>
2021-05-31 17:02:43 +0900

    kotlin basic Loops

---
+++ src/main/kotlin/Basics/Loops.kt
@@ -0,0 +1,26 @@
+package Basics
+
+fun main() {
+    /*
+    while (true) {
+        println("This will print out for a long time!")
+    }
+    */
+
+    val list = listOf(1, 2, 3, 4)
+    for (k in list) {
+        println(k)
+    }
+
+    val set = setOf(1, 2, 3, 4)
+    for (k in set) {
+        println(k)
+    }
+
+    val oneToTen = 1..10
+    for (k in oneToTen) {
+        for (j in 1..5) {
+            println(k * j)
+        }
+    }
+}
