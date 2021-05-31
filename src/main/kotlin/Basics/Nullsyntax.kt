c49a54aca812e98b6105c221357985d5538168ea
EricSeokgon <leesk55@gmail.com>
2021-05-31 17:27:53 +0900

    kotlin basic Explicit casting

--- src/main/kotlin/Basics/Nullsyntax.kt
+++ src/main/kotlin/Basics/Nullsyntax.kt
@@ -1,5 +1,7 @@
 package Basics
 
+import java.io.File
+
 fun main() {
     var str: String? = null
     //var str1: String = null
@@ -21,6 +23,17 @@ fun main() {
     fun isNotStringOrEmpty(any: Any): Boolean {
         return any !is String || any.length == 0
     }
+
+    fun length(any: Any): Int {
+        val string = any as String
+        return string.length
+    }
+
+    //val string: String? = any as String
+
+    val any = "/home/users"
+    val string: String? = any as String
+    val file: File? = any as File
 
 
 }
