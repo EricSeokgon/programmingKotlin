fba0bd5a6c7c706ef58c026290fbbe3e13ce62dd
EricSeokgon <leesk55@gmail.com>
2021-05-31 17:44:54 +0900

    kotlin basic Function Return

--- src/main/kotlin/Basics/Whenexpression.kt
+++ src/main/kotlin/Basics/Whenexpression.kt
@@ -61,6 +61,40 @@ fun main() {
         }
     }
 
+    fun addTwoNumbers(a: Int, b: Int): Int {
+        return a + b
+    }
 
+    fun largestNumber(a: Int, b: Int, c: Int): Int {
+        fun largest(a: Int, b: Int): Int {
+            if (a > b) return a
+            else return b
+        }
+        return largest(largest(a, b), largest(b, c))
+    }
 
+    fun printlessThanTwo() {
+        val list = listOf(1, 2, 3, 4)
+        list.forEach(fun(x) {
+            if (x < 2) println(x)
+            else return
+        })
+        println("This line will still execute")
+    }
+
+    fun printUntilStop() {
+        val list = listOf("a", "b", "stop", "c")
+        list.forEach stop@{
+            if (it == "stop") return@stop
+            else println(it)
+        }
+    }
+
+    fun printUntilStopForEach() {
+        val list = listOf("a", "b", "stop", "c")
+        list.forEach {
+            if (it == "stop") return@forEach
+            else println(it)
+        }
+    }
 }
