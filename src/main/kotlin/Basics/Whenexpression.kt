2edcabe84e183b1b089c9f17ecaabd61447b70e9
EricSeokgon <leesk55@gmail.com>
2021-05-31 17:37:57 +0900

    kotlin basic When without argument

--- src/main/kotlin/Basics/Whenexpression.kt
+++ src/main/kotlin/Basics/Whenexpression.kt
@@ -1,7 +1,5 @@
 package Basics
 
-import sun.security.action.GetBooleanSecurityPropertyAction
-
 fun main() {
     fun whatNumber(x: Int) {
         when (x) {
@@ -12,19 +10,57 @@ fun main() {
     }
 
     fun isMinOrMax(x: Int): Boolean {
-        val isZero=when(x){
-            Int.MIN_VALUE->true
-            Int.MAX_VALUE->true
-            else->false
+        val isZero = when (x) {
+            Int.MIN_VALUE -> true
+            Int.MAX_VALUE -> true
+            else -> false
         }
         return isZero
     }
 
-    fun isZeroOrOne(x:Int):Boolean{
+    fun isZeroOrOne(x: Int): Boolean {
         return when (x) {
             0, 1 -> true
             else -> false
         }
     }
+
+    fun isAbs(x: Int): Boolean {
+        return when (x) {
+            Math.abs(x) -> true
+            else -> false
+        }
+    }
+
+    fun isStringleDigit(x: Int): Boolean {
+        return when (x) {
+            in -9..9 -> true
+            else -> false
+        }
+    }
+
+    fun isDieNumber(x: Int): Boolean {
+        return when (x) {
+            in listOf(1, 2, 3, 4, 5, 6) -> true
+            else -> false
+        }
+    }
+
+    fun startWithFoo(any: Any): Boolean {
+        return when (any) {
+            is String -> any.startsWith("Foo")
+            else -> false
+        }
+    }
+
+    fun whenWithoutArgs(x: Int, y: Int) {
+        when {
+            x < y -> println("x is less than y")
+            x > y -> println("X is greater than y")
+            else -> println("X must equal y")
+        }
+    }
+
+
 
 }
