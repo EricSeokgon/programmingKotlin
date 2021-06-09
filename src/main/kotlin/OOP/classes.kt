53b5629463fb3c4d61620cbd55f9ea6f0bb862ee
EricSeokgon <leesk55@gmail.com>
2021-06-09 17:21:52 +0900

    Classes

---
+++ src/main/kotlin/OOP/classes.kt
@@ -0,0 +1,13 @@
+package OOP
+
+class Deposit {
+}
+
+class Person constructor(val firstName: String, val lastName:  String, val age: Int?) {}
+
+fun main(args: Array<String>) {
+    val person1 = Person("Alex", "Smith", 29)
+    val person2 = Person("Jane", "Smith", null)
+    println("${person1.firstName},${person1.lastName} is  ${person1.age} years old")
+    println("${person2.firstName},${person2.lastName} is  ${person2.age?.toString() ?: "?"} years old")
+}
