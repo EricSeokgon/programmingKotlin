e4afe77e7a78e995b1cd1ac45c2286ed4d2b0a95
EricSeokgon <leesk55@gmail.com>
2021-06-15 18:11:30 +0900

    fizzbuzz

--- src/main/kotlin/Functions/Local.kt
+++ src/main/kotlin/Functions/Local.kt
@@ -13,3 +13,33 @@ fun printArea2(width: Int, height: Int) {
     println("The area is $area")
 }
 
+fun fizzbuzz(start: Int, end: Int): Unit {
+    for (k in start..end) {
+        if (k % 3 == 0 && k % 5 == 0)
+            println("Fizz Buzz")
+        else if (k % 3 == 0)
+            println("Fizz")
+        else if (k % 5 == 0)
+            println("Buzz")
+        else
+            println(k)
+    }
+}
+
+fun fizzbuzz2(start: Int, end: Int): Unit {
+
+    fun isFizz(k: Int): Boolean = k % 3 == 0
+    fun isBuzz(k: Int): Boolean = k % 5 == 0
+
+    for (k in start..end) {
+        if (isFizz(k) && isBuzz(k))
+            println("Fizz Buzz")
+        else if (isFizz(k))
+            println("Fizz")
+        else if (isBuzz(k))
+            println("Buzz")
+        else
+            println(k)
+    }
+}
+
