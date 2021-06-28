2d9f4028de31e802f734cf276b92ad106159a277
EricSeokgon <leesk55@gmail.com>
2021-06-28 14:22:26 +0900

    Named parameters

--- src/main/kotlin/Functions/Namedparameters.kt
+++ src/main/kotlin/Functions/Namedparameters.kt
@@ -1,5 +1,10 @@
 package Functions
 
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
 fun main() {
     val string = "a kindness of ravens"
     string.regionMatches(14, "Red Ravens", 4, 6, true)
@@ -12,3 +17,16 @@ fun main() {
 }
 
 fun deleteFiles(filePattern: String, recursive: Boolean,  ignoreCase: Boolean, deleteDirectories: Boolean): Unit{}
+
+fun createThreadPool(): ExecutorService {
+    val threadCount = Runtime.getRuntime().availableProcessors()
+    return createThreadPool(threadCount)
+}
+
+fun createThreadPool(threadCount: Int): ExecutorService {
+    return Executors.newFixedThreadPool(threadCount)
+}
+
+fun divide(divisor: BigDecimal, scale: Int = 0, roundingMode: RoundingMode = RoundingMode.UNNECESSARY) {}
+
+
