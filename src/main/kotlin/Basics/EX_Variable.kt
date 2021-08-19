b077416f1b8ad887226c362ff2770bf272df74b1
EricSeokgon <leesk55@gmail.com>
2021-08-19 13:18:55 +0900

    변수 사용 예제

---
+++ src/main/kotlin/Basics/EX_Variable.kt
@@ -0,0 +1,16 @@
+package Basics
+
+fun main(args: Array<String>): Unit {
+    var tatal: Int
+    tatal = 0
+
+    val a: Int = 10 + 53 - 7
+    println(a)
+
+    val b: Int = 43 + 75 + a
+    println(b)
+
+    tatal = a + b
+    println(tatal)
+
+}
