ad91a5ad334da76015e51b4b1b53961dbf4041fb
EricSeokgon <leesk55@gmail.com>
2021-08-19 17:44:20 +0900

    흐름 제어 조건문 if - else를 표현식으로 사용하기

---
+++ src/main/kotlin/Basics/EX_IfExpression.kt
@@ -0,0 +1,13 @@
+package Basics
+
+fun main(args: Array<String>) {
+
+    val value = if (10 > 5) {
+        println("10은 5보다 크다.")
+        10
+    } else {
+        println("a")
+        5
+    }
+    println(value)
+}
