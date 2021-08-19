c690a15bcd1a7f1ec65bcbf928ac374bcfd63066
EricSeokgon <leesk55@gmail.com>
2021-08-19 14:04:49 +0900

    논리 연산자 표현식

---
+++ src/main/kotlin/Basics/EX_LogicalOperator.kt
@@ -0,0 +1,12 @@
+package Basics
+
+fun main(args: Array<String>) {
+    val a = 15
+    val b = 17
+
+    var bool: Boolean = (a - b < a + b) && (a == 15)
+    println(bool)
+
+    bool = !((a + b) > (a * 3) || (b - a) > 0)
+    println(bool)
+}
