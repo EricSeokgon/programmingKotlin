f8ab1796250bb123dcf5b65a372efc13558f84d0
EricSeokgon <leesk55@gmail.com>
2021-08-23 17:22:03 +0900

    소스 파일 여러개로 분리하기 예제

---
+++ src/main/kotlin/Basics/EX_SplitSourceFile.kt
@@ -0,0 +1,8 @@
+package Basics
+
+fun main(args: Array<String>) {
+    val a = 20
+    val b = -30
+
+    println(max(a, abs(b)))
+}
---
+++ src/main/kotlin/Basics/Math.kt
@@ -0,0 +1,9 @@
+package Basics
+
+fun max(a: Int, b: Int): Int = if (a > b) a else b
+
+fun min(a: Int, b: Int): Int = if (a < b) a else b
+
+fun abs(num: Int): Int = if (num >= 0) num else -num
+
+
