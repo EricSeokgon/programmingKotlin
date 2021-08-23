324896a6b9c48ad8234d7b71fe2839d1a2a2859f
EricSeokgon <leesk55@gmail.com>
2021-08-23 17:15:42 +0900

    지역함수 예제

---
+++ src/main/kotlin/Basics/EX_LocalFunction.kt
@@ -0,0 +1,10 @@
+package Basics
+
+fun main(args: Array<String>) {
+    fun printFomular(a: Int, b: Int) {
+        println(a * b + a - b)
+    }
+
+    printFomular(73, 1)
+    printFomular(4, 6)
+}
