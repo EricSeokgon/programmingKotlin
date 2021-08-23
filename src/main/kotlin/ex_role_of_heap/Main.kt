02561942f7dd65e871219e4779ccb7670683b005
EricSeokgon <leesk55@gmail.com>
2021-08-23 17:40:21 +0900

    힙 영역의 존재 이유

---
+++ src/main/kotlin/ex_role_of_heap/Main.kt
@@ -0,0 +1,26 @@
+package ex_role_of_heap
+
+fun main(args: Array<String>) {
+    val product: Product;
+    product = createProduct()
+    printProductInfo(product)
+    processProduct(product)
+    printProductInfo(product)
+}
+
+fun printProductInfo(product: Product) {
+    println("이름: ${product.name}")
+    println("가격: ${product.price}")
+}
+
+fun processProduct(product: Product) {
+    product.price += 500
+}
+
+fun createProduct(): Product {
+    val apple = Product()
+    apple.name = "Apple"
+    apple.price = 1000
+
+    return apple
+}
