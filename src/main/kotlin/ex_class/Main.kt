4235bd4fe49fcf0bb09c224d62f51ee0f6ecc97a
EricSeokgon <leesk55@gmail.com>
2021-08-23 17:35:21 +0900

    클래스 객체 사용 예제

--- src/main/kotlin/ex_class/Main.kt
+++ src/main/kotlin/ex_class/Main.kt
@@ -12,10 +12,10 @@ fun main(args: Array<String>) {
     person.age = 36
 
     val person2 = Person()
-    person.name = "홍길동"
-    person.age = 36
+    person.name = "김미영"
+    person.age = 29
 
     val person3 = Person()
-    person.name = "홍길동"
-    person.age = 36
+    person.name = "John"
+    person.age = 52
 }
