d57ef61101a5ddd4c31e561b4307ab6ef3d874f8
EricSeokgon <leesk55@gmail.com>
2021-10-01 16:28:28 +0900

    다이아몬드 문제 : 상속 관계 그림이 다이아몬드를 닮아서 다이아몬드 문제라는 이름이 붙었다.

---
+++ src/main/kotlin/ex_the_diamond_problem/Main.kt
@@ -0,0 +1,24 @@
+package ex_the_diamond_problem
+
+interface Parent {
+    fun follow(): Unit
+}
+
+interface Mother : Parent {
+    override fun follow() = println("follow his mother")
+}
+
+interface Father : Parent {
+    override fun follow() = println("follow his father")
+}
+
+class Child : Mother, Father {
+    override fun follow() {
+        println("A child decided to")
+        super<Father>.follow()
+    }
+}
+
+fun main(args: Array<String>) {
+    Child().follow()
+}
