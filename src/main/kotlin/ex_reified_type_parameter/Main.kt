a826cd772d97a450c7e82583951c50f48a6254ec
EricSeokgon <leesk55@gmail.com>
2021-10-07 17:53:13 +0900

    구체화된(reified)타입 매개변수

---
+++ src/main/kotlin/ex_reified_type_parameter/Main.kt
@@ -0,0 +1,11 @@
+package ex_reified_type_parameter
+
+inline fun <reified T> check() {
+    val number = 0
+    if (number is T)
+        println("T는 Int 타입 입니다.")
+}
+
+fun main(args: Array<String>) {
+    check<Int>()
+}
