bd1a2ef2fd3234b9a8168ced9dd5192c67b37082
EricSeokgon <leesk55@gmail.com>
2021-10-07 16:36:10 +0900

    함수 참조 : 함수 타입의 변수는 이미 선언되어 있는 함수나 객체의 멤버 함수를 가리킬 수도 있다.

--- src/main/kotlin/ex_anonymous_function/Main.kt
+++ src/main/kotlin/ex_anonymous_function/Main.kt
@@ -2,7 +2,7 @@ package ex_anonymous_function
 
 fun main(args: Array<String>) {
     val instantFunc: (Int) -> Unit = fun(number: Int): Unit {
-        println("Hello @number")
+        println("Hello $number")
     }
 
     instantFunc(33)
