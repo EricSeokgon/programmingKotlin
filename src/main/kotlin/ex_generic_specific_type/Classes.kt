238816c11c62c35fceb894d680df75e89bffc536
EricSeokgon <leesk55@gmail.com>
2021-10-08 10:15:05 +0900

    valueContainer 인터페이스를 구현하는 클래스들을 선언

--- src/main/kotlin/ex_generic_specific_type/Classes.kt
+++ src/main/kotlin/ex_generic_specific_type/Classes.kt
@@ -3,3 +3,11 @@ package ex_generic_specific_type
 interface ValueContainer {
     fun getValue(): Int
 }
+
+class AAA : ValueContainer {
+    override fun getValue(): Int = 1102
+}
+
+class BBB : ValueContainer {
+    override fun getValue(): Int = 127
+}
