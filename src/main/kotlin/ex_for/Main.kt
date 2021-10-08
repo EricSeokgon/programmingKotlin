6db05c9d5fe9bb0082f2cb54053d6de54a0423ac
EricSeokgon <leesk55@gmail.com>
2021-10-08 11:01:26 +0900

    흐림 제어 반복문 for 사용 예제

---
+++ src/main/kotlin/ex_for/Main.kt
@@ -0,0 +1,15 @@
+package ex_for
+
+fun main(args: Array<String>) {
+    for (i: Int in 1..10) {
+        print("$i ")
+    }
+    println()
+
+    for (i: Int in 1..10) {
+        if (i > 5) {
+            break
+        }
+        print("$i ")
+    }
+}
