2ee1ee1c392b28373be3cdc0eb952543faffb213
EricSeokgon <leesk55@gmail.com>
2021-10-22 10:38:26 +0900

    map 인터페이스 사용예제

---
+++ src/main/kotlin/part4/ex_map_interface/Main.kt
@@ -0,0 +1,15 @@
+package part4.ex_map_interface
+
+fun main(args: Array<String>) {
+    val map: Map<String, String> = mapOf("Apple" to "사과", "Banana" to "바나나")
+
+    println(map.size)
+    println(map.keys)
+    println(map.values)
+    println(map.entries)
+    println(map.isEmpty())
+    println(map.containsKey("Cocoa"))
+    println(map.containsValue("Cocoa"))
+    println(map["Apple"])
+    println(map.getOrDefault("Cocoa", "코코아"))
+}
