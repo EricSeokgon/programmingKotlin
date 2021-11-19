11da05a940a7b826e2230c27aede441e16d1e73f
EricSeokgon <leesk55@gmail.com>
2021-11-19 17:34:49 +0900

    CharSequence 인터페이스 : 문자열과 관려니된 클래스가 구현하는 뼈대 인터페이스

---
+++ src/main/kotlin/part4/ex_char_sequence_interface/Main.kt
@@ -0,0 +1,8 @@
+package part4.ex_char_sequence_interface
+
+fun main(args: Array<String>) {
+    val seq: CharSequence = "Hello"
+    println(seq.length)
+    println(seq[2])
+    println(seq.subSequence(1, 4))
+}
